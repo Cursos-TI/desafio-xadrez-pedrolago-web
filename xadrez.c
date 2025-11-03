c2411301666ba2865742c9114cc3f984fc7b469c
pedrolago-web <pedrolago2001@gmail.com>
2025-11-03 17:31:03 +0000

    AddCavalo

--- xadrez.c
+++ xadrez.c
@@ -5,25 +5,18 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    // Nível Aventureiro - Movimentação do Cavalo
+    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
+    // Um loop pode representar a movimentação horizontal e outro vertical.
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    printf("Movimento da Torre:\n");
+        printf("Movimento da Torre:\n");
     int casas_torre = 5;
     for(int i = 0; i < casas_torre; i++) {
         printf("Direita\n");
     }
 
-    //Bispo: 
+    // Bispo: 
     printf("\nMovimento do Bispo:\n");
     int casas_bispo = 5;
     int contador_bispo = 0;
@@ -32,7 +25,7 @@ int main() {
         contador_bispo++;
     }
 
-    //Rainha:
+    // Rainha: 
     printf("\nMovimento da Rainha:\n");
     int casas_rainha = 8;
     int contador_rainha = 0;
@@ -40,11 +33,33 @@ int main() {
         printf("Esquerda\n");
         contador_rainha++;
     } while(contador_rainha < casas_rainha);
+
+    // Cavalo: 
+    printf("\nMovimento do Cavalo:\n");
     
+    // Movimento Cavalo: 
+    int casas_baixo = 2;
+    int casas_esquerda = 1;
     
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    for(int movimento_principal = 0; movimento_principal < casas_baixo; movimento_principal++) {
+        
+        printf("Baixo\n");
+        
+        int movimento_perpendicular = 0;
+        while(movimento_perpendicular < casas_esquerda) {
+            
+            if(movimento_principal == casas_baixo - 1) {
+                printf("Esquerda\n");
+                movimento_perpendicular++;
+            } else {
+                
+                break;
+            }
+        }
+    }
+
+
+
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
