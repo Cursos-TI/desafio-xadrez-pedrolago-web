dd178c117fc7c7b693fc1a74f5f541ebae7f6c6e
pedrolago-web <pedrolago2001@gmail.com>
2025-11-03 17:41:29 +0000

    Alteração

--- xadrez.c
+++ xadrez.c
@@ -37,8 +37,26 @@ void moverRainha(int casas) {
     moverRainha(casas - 1);
 }
 
+void moverBispoLoopsAninhados(int casas) {
+    printf("Movimento do Bispo com Loops Aninhados:\n");
+    
+    for (int vertical = 0; vertical < casas; vertical++) {
+        
+        for (int horizontal = 0; horizontal < 1; horizontal++) {
+        
+            printf("Cima, Direita\n");
+        }
+    }
+}
 
-
+void moverCavaloComplexo() {
+    printf("Movimento do Cavalo (Cima e Direita):\n");
+    
+    int casas_cima = 2;
+    int casas_direita = 1;
+    int movimento_completo = 0;
+    
+    for (int etapa = 0; etapa < casas_cima + casas_direita; etapa++) {
 
 
 
